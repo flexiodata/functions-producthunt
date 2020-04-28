7fedad441c6edd2f75419b0d63550c92a8045a1c
Aaron Williams <aaron@flex.io>
2020-04-28 15:04:26 -0500

    added fault-tolerant request mechanism

--- producthunt-posts.py
+++ producthunt-posts.py
@@ -37,8 +37,10 @@
 # ---
 
 import json
-import requests
 import urllib
+import requests
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
 import itertools
 from datetime import *
 from decimal import *
@@ -101,7 +103,7 @@ def flexio_handler(flex):
         columns = list(property_map.values())
         data = { "query": "query { posts { edges { node {" + " ".join(columns) + " } } } }" }
 
-        response = requests.post(url, data=json.dumps(data), headers=headers)
+        response = requests_retry_session().post(url, data=json.dumps(data), headers=headers)
         response.raise_for_status()
         content = response.json()
 
@@ -127,6 +129,25 @@ def flexio_handler(flex):
     except:
         flex.output.content_type = 'application/json'
         flex.output.write([['']])
+
+def requests_retry_session(
+    retries=3,
+    backoff_factor=0.3,
+    status_forcelist=(500, 502, 504),
+    session=None,
+):
+    session = session or requests.Session()
+    retry = Retry(
+        total=retries,
+        read=retries,
+        connect=retries,
+        backoff_factor=backoff_factor,
+        status_forcelist=status_forcelist,
+    )
+    adapter = HTTPAdapter(max_retries=retry)
+    session.mount('http://', adapter)
+    session.mount('https://', adapter)
+    return session
 
 def validator_list(field, value, error):
     if isinstance(value, str):
